c7542e45db95a42f6bc09c826c438a30a3fcaae0
samaksh <samakshsangwan@gmail.com>
2025-12-14 15:15:37 +0530

    minor changes

--- src/main/java/com/samaksh/flyway/controller/flywayController.java
+++ src/main/java/com/samaksh/flyway/controller/flywayController.java
@@ -1,10 +1,14 @@
 package com.samaksh.flyway.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1")
 public class flywayController {
+    @Autowired
+    random ran;
+
     @GetMapping("/get")
     String getname(@RequestParam(value = "name") String user){
         return "welcome "+user+" !!";
---
+++ src/main/java/com/samaksh/flyway/controller/random.java
@@ -0,0 +1,11 @@
+package com.samaksh.flyway.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class random {
+
+    random(){
+        int a=5;
+    }
+}
