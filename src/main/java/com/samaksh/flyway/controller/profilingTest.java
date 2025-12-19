88770e0cabb4d9f7ecb3bc9f6705e4f78836d026
samaksh <samakshsangwan@gmail.com>
2025-12-19 23:03:39 +0530

    minor changes

--- src/main/java/com/samaksh/flyway/controller/profilingTest.java
+++ src/main/java/com/samaksh/flyway/controller/profilingTest.java
@@ -1,13 +1,20 @@
 package com.samaksh.flyway.controller;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class profilingTest {
 
+    @Value("${username}")
+    String username;
+
+    @Value("${password}")
+    String password;
+
     @PostConstruct
     public void fun(){
-        System.out.println();
+        System.out.println(username + " " + password);
     }
 }
